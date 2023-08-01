1fe5a74b9d3c8c7d7477fcc72bc8160d9c68773f
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-07-31 22:25:28 -0300

    feat: classe que faz a conexão com o googleAPI

---
+++ organizador_emails/google_api/connection.py
@@ -0,0 +1,53 @@
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+
+
+class ConnectGoogleAPI:
+    def __init__(self):
+        self.creds = None
+
+
+    def get_credentials(self):
+        # The file token.json stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists('keys/token.json'):
+            self.creds = Credentials.from_authorized_user_file('keys/token.json', SCOPES)
+
+        # If there are no (valid) credentials available, let the user log in.
+        if not self.creds or not self.creds.valid:
+            if self.creds and self.creds.expired and self.creds.refresh_token:
+                self.creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'keys/credentials.json', SCOPES)
+                self.creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('keys/token.json', 'w') as token:
+                token.write(self.creds.to_json())
+
+
+    def get_labels(self):
+        try:
+            # Call the Gmail API
+            service = build('gmail', 'v1', credentials=self.creds)
+            results = service.users().labels().list(userId='me').execute()
+            labels = results.get('labels', [])
+
+            if not labels:
+                print('No labels found.')
+                return
+            print('Labels:')
+            for label in labels:
+                print(label['name'])
+
+        except HttpError as error:
+            # TODO(developer) - Handle errors from gmail API.
+            print(f'An error occurred: {error}')
