0197051059cc8d08c9eb3ac529154f0346a6dd09
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-07-31 22:31:46 -0300

    feat: classe principal para rodar o programa

---
+++ organizador_emails/main.py
@@ -0,0 +1,15 @@
+from google_api.connection import ConnectGoogleAPI
+import os
+import sys
+
+
+class Main:
+    @staticmethod
+    def main():
+        google_api = ConnectGoogleAPI()
+        google_api.get_credentials()
+        google_api.get_labels()
+
+
+if __name__ == '__main__':
+    Main.main()
