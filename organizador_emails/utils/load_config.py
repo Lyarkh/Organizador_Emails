9acaeb3a5e9650e8832a337bafbb502d9dba79bb
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-08-05 23:09:11 -0300

    feat: adicionando logica base da leitura do arquivo de configuração

--- organizador_emails/utils/load_config.py
+++ organizador_emails/utils/load_config.py
@@ -1,14 +1,23 @@
-import os, sys, pathlib
+import os, sys
 import yaml
 
 
 class LoadConfig:
     def __init__(self):
-        self.prepare_paths()
+        self.add_project_path()
 
+    def add_project_path(self):
+        sys.path.insert(0, os.getcwd())
 
-    def prepare_paths(self):
-        print(sys.path.insert(0, os.getcwd()))
+    def run(self):
+        yaml_config = self.open_yaml_file()
+
+        return yaml_config
+
+    def open_yaml_file(self):
+        with open('config.yaml', 'r') as file:
+            return yaml.safe_load(file)
+
 
 if __name__ == '__main__':
-    LoadConfig()
+    LoadConfig().run()
