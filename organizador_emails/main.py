aa15b561040224d38dee4c813a42ac5fe1ba6b55
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-08-15 23:20:11 -0300

    feat: modificação teste para busca de todas as mensagens utilizando while

--- organizador_emails/main.py
+++ organizador_emails/main.py
@@ -6,8 +6,18 @@ class Main:
     def main(configs):
         google_api = ConnectGoogleAPI(configs)
         google_api.get_credentials()
-        all_messages = google_api.get_messages_list([], '')
-        print(f'Quantidade de mensagens encontradas: {len(all_messages)}')
+
+        all_messages = []
+        page_token = ''
+
+        while True:
+            find_messages, next_token = google_api.get_messages_list(page_token)
+            all_messages.extend(find_messages)
+            page_token = next_token
+            print(f'Quantidade de mensagens encontradas: {len(all_messages)}')
+            print(f'Próximo token:  {page_token}')
+            if not page_token:
+                break
 
         import json
 
