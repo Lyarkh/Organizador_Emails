c20ac33d4c4e6fcba49c3746c5c5f2b841f8c345
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-08-17 10:17:01 -0300

    feat: fazendo a chamada para busca de cada uma das mensagens

--- organizador_emails/main.py
+++ organizador_emails/main.py
@@ -7,23 +7,30 @@ class Main:
         google_api = ConnectGoogleAPI(configs)
         google_api.get_credentials()
 
+        all_id_messages = []
         all_messages = []
         page_token = ''
 
         while True:
             find_messages, next_token = google_api.get_messages_list(page_token)
-            all_messages.extend(find_messages)
+            all_id_messages.extend(find_messages)
             page_token = next_token
-            print(f'Quantidade de mensagens encontradas: {len(all_messages)}')
-            print(f'Próximo token:  {page_token}')
+            print(f'Quantidade de mensagens encontradas: {len(all_id_messages)}')
             if not page_token:
                 break
 
-        import json
+        for message in all_id_messages:
+            print(f"Buscando info da message com id: {message['id']}")
+            message_info = google_api.get_info_message(message['id'])
+            all_messages.append(message_info)
 
-        with open('results.json', mode='w') as file:
-            json.dump(all_messages, file, indent=4)
+        return all_messages
 
 
 if __name__ == '__main__':
-    Main.main()
+    all_messages = Main.main()
+
+    import json
+
+    with open('results.json', mode='w') as file:
+        json.dump(all_messages, file, indent=4)
