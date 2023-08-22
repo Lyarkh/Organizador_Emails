c92f1ae70b67a96d37b6338300a34864d95f57b4
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-08-21 21:38:53 -0300

    fix: correão data_parser, para aceitar novas variedades de dados

--- organizador_emails/utils/date_parser.py
+++ organizador_emails/utils/date_parser.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 
-
 class DateParser:
     def __init__(self, date_string):
         self.date_string = date_string
@@ -8,6 +7,18 @@ class DateParser:
 
     def parse(self):
         # Example of the format -> 'Thu, 3 Aug 2023 19:18:55 +0000 (UTC)'
-        temp_date = self.date_string.split(',')
-        date = datetime.strptime(temp_date[1], '%d %b %Y %H:%M:%S %z (UTC)')
+        print(self.date_string)
+        temp_date = self.date_string.split(', ')[1]
+
+        flag_offset = '+'
+        if '-' in temp_date:
+            flag_offset = '-'
+
+        print(temp_date)
+        temp_date_without_offset = temp_date.split(flag_offset)[0].strip()
+        print(temp_date_without_offset)
+
+        str_format_date = '%d %b %Y %H:%M:%S'
+
+        date = datetime.strptime(temp_date_without_offset, str_format_date)
         setattr(self, 'date', date)
