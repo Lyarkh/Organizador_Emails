54ca66f2c756f64d41884778a529df607ebd7a6b
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-08-21 22:11:17 -0300

    fix: correção de bugs da classe data_parser

--- organizador_emails/utils/date_parser.py
+++ organizador_emails/utils/date_parser.py
@@ -4,7 +4,6 @@ from datetime import datetime
 class DateParser:
     def __init__(self, date_string):
         self.date_string = date_string
-        self.parse()
 
     def parse(self):
         # Example of the format -> 'Thu, 3 Aug 2023 19:18:55 +0000 (UTC)'
@@ -14,17 +13,17 @@ class DateParser:
         self.create_date_object(clean_date)
 
     def clean_the_date_string(self, date_str_to_clean):
-        if ',' in temp_date:
-            temp_date = self.date_string.split(', ')[1]
+        if ',' in date_str_to_clean:
+            date_str_to_clean = self.date_string.split(', ')[1]
 
         flag_offset = '+'
-        if '-' in temp_date:
+        if '-' in date_str_to_clean:
             flag_offset = '-'
 
-        temp_date_without_offset = temp_date.split(flag_offset)[0].strip()
+        temp_date_without_offset = date_str_to_clean.split(flag_offset)[0].strip()
         return temp_date_without_offset
 
-    def create_date_obj(self, str_date_to_convert):
+    def create_date_object(self, str_date_to_convert):
         str_format_date = '%d %b %Y %H:%M:%S'
         date = datetime.strptime(str_date_to_convert, str_format_date)
         setattr(self, 'date', date)
