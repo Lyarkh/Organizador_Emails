16b9caffae1c5d1f44f5b76b79c8389120e9457b
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-08-25 21:57:28 -0300

    refactor: retirando parâmetro desnecessário

--- organizador_emails/google_api/service_interface.py
+++ organizador_emails/google_api/service_interface.py
@@ -4,9 +4,8 @@ from organizador_emails.google_api.api_connection import APIConnection
 
 
 class ServiceInterface:
-    def __init__(self, name, configs):
+    def __init__(self, configs):
         self.configs = configs
-        self.name_service = name
         self.creds = APIConnection(configs).get_credentials()
 
     def get_email_service(self):
