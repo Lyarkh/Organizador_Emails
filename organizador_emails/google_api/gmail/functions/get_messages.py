0d25f36f34c267a4afc57119594d546ef4b101e1
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-08-25 21:48:30 -0300

    feat: base da classe de busca das mensagens

---
+++ organizador_emails/google_api/gmail/functions/get_messages.py
@@ -0,0 +1,57 @@
+
+
+class Getmessages:
+    def get_messages_list(self, page_token):
+        service = self.get_service()
+        messages = (
+            service.users()
+            .messages()
+            .list(userId='me', pageToken=page_token)
+            .execute()
+        )
+
+        result = messages['messages']
+        new_page_token = messages.get('nextPageToken', '')
+
+        return result, new_page_token
+
+    def get_info_message(self, message_id):
+        service = self.get_service()
+        message = (
+            service.users()
+            .messages()
+            .get(userId='me', id=message_id)
+            .execute()
+        )
+        self.get_headers_info(message)
+
+        return message
+
+    def get_headers_info(self, message):
+        header_info = message['payload']['headers']
+
+        labels = message['labelIds']
+        message_snippet = message['snippet']
+
+        user_from = 'User Send - Not Found'
+        date = 'Date - Not Found'
+        subject = 'Subject - Not Found'
+
+        for info in header_info:
+            if info['name'] == 'From':
+                user_from = info['value']
+            if info['name'] == 'Date':
+                date_parser = DateParser(info['value'])
+                date = date_parser.date
+            if info['name'] == 'Subject':
+                subject = info['value']
+
+        print(
+            '\n------------------\n'
+            f'From: {user_from}\n'
+            f'Labels: {labels}\n'
+            f'Date: {date}\n'
+            f'Subject: {subject}\n'
+            f'Snippet: {message_snippet}\n'
+            '------------------'
+        )
