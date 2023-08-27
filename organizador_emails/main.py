eb2ecff01196c608093f335a5bec45edc4e16c4a
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-08-26 21:49:20 -0300

    style: formatação de código para a PEP8 e seguindo padrão blue

--- organizador_emails/main.py
+++ organizador_emails/main.py
@@ -1,5 +1,8 @@
+from organizador_emails.google_api.gmail.functions.get_messages import (
+    Getmessages,
+)
 from organizador_emails.google_api.service_interface import ServiceInterface
-from organizador_emails.google_api.gmail.functions.get_messages import Getmessages
+
 
 class Main:
     @staticmethod
@@ -13,7 +16,9 @@ class Main:
         MAX = 1000
 
         while True:
-            find_messages, next_token = Getmessages().get_messages_list(google_api, page_token)
+            find_messages, next_token = Getmessages().get_messages_list(
+                google_api, page_token
+            )
             all_id_messages.extend(find_messages)
             page_token = next_token
             print(
@@ -28,7 +33,9 @@ class Main:
 
         for message in all_id_messages:
             print(f"Buscando info da message com id: {message['id']}")
-            message_info = Getmessages().get_info_message(google_api, message['id'])
+            message_info = Getmessages().get_info_message(
+                google_api, message['id']
+            )
             all_messages.append(message_info)
 
         return all_messages
