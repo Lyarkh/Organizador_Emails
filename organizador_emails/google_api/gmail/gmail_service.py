b125c9f1335e4a887147d38844970dc37b06f666
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-08-27 22:07:59 -0300

    refactor: modificando nome do property

--- organizador_emails/google_api/gmail/gmail_service.py
+++ organizador_emails/google_api/gmail/gmail_service.py
@@ -9,7 +9,7 @@ class GmailService:
         self.creds = APIConnection(self.configs).get_credentials()
 
     @property
-    def get_build(self):
+    def build(self):
         return build(
             self.configs.google.services.gmail.name,
             self.configs.google.services.gmail.version,
