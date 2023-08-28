321d9e95980c7c8f7cd1ce8d4b5cffa59087011c
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-08-27 21:10:51 -0300

    refactor: modificando chamada de busca do build do gmail service

--- organizador_emails/google_api/service_interface.py
+++ organizador_emails/google_api/service_interface.py
@@ -1,14 +1,10 @@
-from organizador_emails.google_api.api_connection import APIConnection
+from organizador_emails.google_api.gmail.gmail_service import GmailService
 
 
 class ServiceInterface:
     def __init__(self, configs):
         self.configs = configs
-        self.creds = APIConnection(configs).get_credentials()
+
 
     def get_email_service(self):
-        return build(
-            self.configs.google.services.gmail.name,
-            self.configs.google.services.gmail.version,
-            credentials=self.creds,
-        )
+        return GmailService().build()
