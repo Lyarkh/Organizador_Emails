88cdfa847028fd47aed97395083d07f18eb1e4db
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-08-27 22:15:15 -0300

    feat: adicionando classe mãe para validação dos métodos necessários

--- organizador_emails/google_api/gmail/gmail_service.py
+++ organizador_emails/google_api/gmail/gmail_service.py
@@ -1,9 +1,10 @@
 from googleapiclient.discovery import build
 
 from organizador_emails.google_api.api_connection import APIConnection
+from organizador_emails.models.base_service import BaseService
 
 
-class GmailService:
+class GmailService(BaseService):
     def __init__(self, configs):
         self.configs = configs
         self.creds = APIConnection(self.configs).get_credentials()
