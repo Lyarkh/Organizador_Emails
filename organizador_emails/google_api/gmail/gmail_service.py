6fce46bb35ffb612ba4f86d79dd7cbf6887cac62
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-08-27 21:12:07 -0300

    feat: criando construtor da classe GmailService para buscar as credenciais

--- organizador_emails/google_api/gmail/gmail_service.py
+++ organizador_emails/google_api/gmail/gmail_service.py
@@ -1,7 +1,12 @@
+from organizador_emails.google_api.api_connection import APIConnection
 from googleapiclient.discovery import build
 
 
 class GmailService:
+    def __init__(self, configs):
+        self.configs = configs
+        self.creds = APIConnection(configs).get_credentials()
+
     def build(self):
         return build(
             self.configs.google.services.gmail.name,
