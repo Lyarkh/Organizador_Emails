fb94ca68e3cf067a61e5b1ba6215f302f7712c77
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-09-02 19:28:11 -0300

    feat: adicionando imports dos serviços

--- organizador_emails/__main__.py
+++ organizador_emails/__main__.py
@@ -4,6 +4,10 @@ import sys
 sys.path.insert(0, os.getcwd())
 
 from organizador_emails.utils.load_config import LoadConfig
+from organizador_emails.google_api.gmail.functions.get_messages import (
+    Getmessages,
+)
+from organizador_emails.google_api.service_interface import ServiceInterface
 
 configs = LoadConfig()
 
