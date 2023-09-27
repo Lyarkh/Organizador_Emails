d4d5d2aea5df444ef218de349a5fc23308bb4ed6
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-09-27 20:48:45 -0300

    feat: criando base tabela de emails

--- organizador_emails/models/db/emails.py
+++ organizador_emails/models/db/emails.py
@@ -1,6 +1,16 @@
 from typing import List, Optional
+from datetime import datetime
 from sqlalchemy import String
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 class Base(DeclarativeBase):
     ...
+
+
+class Emails(Base):
+    __tablename__ = 'emails'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    subject: Mapped[str] = mapped_column(String(200))
+    user_from: Mapped[str] = mapped_column(String(200))
+    date: Mapped[datetime] = mapped_column(datetime)
