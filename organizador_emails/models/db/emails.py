a5f28797ba67a8780839e91bb0fac5a0feec32b8
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-09-27 20:49:19 -0300

    style: formatação de código

--- organizador_emails/models/db/emails.py
+++ organizador_emails/models/db/emails.py
@@ -1,7 +1,9 @@
-from typing import List, Optional
 from datetime import datetime
+from typing import List, Optional
+
 from sqlalchemy import String
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
 
 class Base(DeclarativeBase):
     ...
