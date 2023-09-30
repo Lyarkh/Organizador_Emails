760cdb96a09e4e35fae47f91f9973624cba99c4b
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-09-29 21:32:28 -0300

    feat/refactor: separando a função de execução para encontrar os emais

--- organizador_emails/__main__.py
+++ organizador_emails/__main__.py
@@ -14,42 +14,43 @@ from organizador_emails.db.connect import engine
 Base.metadata.create_all(bind=engine)
 
 
-from organizador_emails.google_api.gmail.functions.get_messages import (
-    Getmessages,
-)
-from organizador_emails.google_api.service_interface import ServiceInterface
-
-gmail_service = ServiceInterface.get_email_service(configs)
-
-all_id_messages = []
-all_messages = []
-page_token = ''
-
-MAX = configs.vars.max_messages
-
-while True:
-    find_messages, next_token = Getmessages().get_messages_list(
-        gmail_service.build, page_token
+def exec_find_emails():
+    from organizador_emails.google_api.gmail.functions.get_messages import (
+        Getmessages,
     )
-    all_id_messages.extend(find_messages)
-    page_token = next_token
-    print(f'Quantidade de mensagens encontradas: {len(all_id_messages)}')
+    from organizador_emails.google_api.service_interface import ServiceInterface
 
-    if len(all_id_messages) >= MAX:
-        break
+    gmail_service = ServiceInterface.get_email_service(configs)
 
-    if not page_token:
-        break
+    all_id_messages = []
+    all_messages = []
+    page_token = ''
 
-for message in all_id_messages:
-    print(f"Buscando info da message com id: {message['id']}")
-    message_info = Getmessages().get_info_message(
-        gmail_service.build, message['id']
-    )
-    all_messages.append(message_info)
+    MAX = configs.vars.max_messages
+
+    while True:
+        find_messages, next_token = Getmessages().get_messages_list(
+            gmail_service.build, page_token
+        )
+        all_id_messages.extend(find_messages)
+        page_token = next_token
+        print(f'Quantidade de mensagens encontradas: {len(all_id_messages)}')
+
+        if len(all_id_messages) >= MAX:
+            break
+
+        if not page_token:
+            break
+
+    for message in all_id_messages:
+        print(f"Buscando info da message com id: {message['id']}")
+        message_info = Getmessages().get_info_message(
+            gmail_service.build, message['id']
+        )
+        all_messages.append(message_info)
 
 
-import json
+    import json
 
-with open('results.json', mode='w') as file:
-    json.dump(all_messages, file, indent=4)
+    with open('results.json', mode='w') as file:
+        json.dump(all_messages, file, indent=4)
